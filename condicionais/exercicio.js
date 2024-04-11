c371a42ffd4fd0b402a6738f724fb06d0361a8ba
GabrielPIAUI <gabriel.r.machado6@aluno.senai.br>
2024-04-11 10:42:05 -0300

    JS Aula 7

---
+++ condicionais/exercicio.js
@@ -0,0 +1,8 @@
+var vel = 59.9
+console.log(`A velocidade do seu carro é ${vel}`)
+if (vel > 60) {
+    console.log('Você ultrapassou a velocidade permitida, MULTADO')}
+else if (vel < 60) {
+    console.log('Você está dentro da velocidade permitida')
+}
+console.log('Dirija sempre com o cinto de segurança')
